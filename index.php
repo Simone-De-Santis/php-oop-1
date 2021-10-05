eb61a364ef09a6bc80d0be3bbff9b18071a76bdc
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-10-05 17:57:35 +0200

    creazione altra funzione con stampa via eco ...->

--- index.php
+++ index.php
@@ -19,6 +19,20 @@ class Movie
     public function saluta()
     {
         echo 'io sono la funzione di ' . $this->original_title . "</br>";
+        echo 'il mio voto è ' . $this->vote_average . "</br>";
+    }
+
+
+    //? questa funzione restituisce un valore che va stampato ->>>>>
+    // !segui il rosso _-_-_->>
+    public function getMyJudment()
+    {
+        if ($this->vote_average > 6) {
+            $this->my_judment = 'troppo bello ';
+        } else {
+            $this->my_judment = 'troppo brutto ';
+        }
+        return $this->my_judment;
     }
 }
 
@@ -26,11 +40,12 @@ class Movie
 // instance 1
 $pluto = new Movie();
 $pluto->id = "99999";
-$pluto->original_title = "il signore degli anelli ";
+$pluto->original_title = "Il signore degli anelli ";
 $pluto->original_language = "it";
 $pluto->overview = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta maxime molestiae architecto ad dolorem cumque iste in eum rerum illo. Laboriosam dolor hic suscipit officia expedita a illo maxime necessitatibus.";
-$pluto->vote_average = "5.5";
-$pluto->my_judment = "";
+$pluto->vote_average = 5;
+//! chiamo la funzione che verrà stampata grazie al var_dump
+$pluto->getMyJudment();
 
 // instance 2
 $paperino = new Movie();
@@ -38,14 +53,17 @@ $paperino->id = "11111";
 $paperino->original_title = "The Matrix";
 $paperino->original_language = "it";
 $paperino->overview = "Consectetur adipisicing elit. Dicta maxime molestiae architecto ad dolorem cumque iste in eum rerum illo. Laboriosam dolor hic suscipit officia expedita a 'So Lillo'.";
-$paperino->vote_average = "9.9";
-$paperino->my_judment = "";
+$paperino->vote_average = 9;
+//! chiamo la funzione che verrà stampata grazie al var_dump
+$paperino->getMyJudment();
 
 var_dump($pluto);
 var_dump($paperino);
 $pluto->saluta();
 $paperino->saluta();
 
-echo 'il nome del mio film preferito è : ' . $paperino->original_title
+echo 'il nome del mio film preferito è : ' . $paperino->original_title . "</br>";
+echo 'il mio giudizio sul film è : ' . $paperino->getMyJudment() . "</br>";
+
 
 ?>
