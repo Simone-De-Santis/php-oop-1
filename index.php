bf62096d3e4a2f87bbdab67c3de8a713622ce21a
Simone-De-Santis <simone.agentelazio@yahoo.it>
2021-10-05 16:35:50 +0200

    creazione file php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!-- è definita una classe ‘Movie’
+=> all'interno della classe sono dichiarate delle variabili d'istanza (proprietà)
+=> all'interno della classe è definito un costruttore
+=> all'interno della classe è definito almeno un metodo
+- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà, se possibile cerchiamo di stampare senza ricorrere a var_dump
+-->
+<?php
+echo 'ciao '
+
+?>
